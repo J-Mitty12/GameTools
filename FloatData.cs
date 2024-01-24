cc2ac0ddb195b28ac415ceb3926dc1d2c62de953
Julian Mitton <jdmitton12@gmail.com>
2024-01-24 09:51:30 -0700

    Update to FloatData

--- FloatData.cs
+++ FloatData.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
-public class FloatData : MonoBehaviour
+public class FloatData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent onZeroEvent;
+
+    public float value;
+
+    public void UpdateValue(float number)
     {
-        
+        value += number;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReplaceValue(float number)
     {
-        
+        value = number;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        if (value <= 0)
+        {
+            onZeroEvent.Invoke();
+        }   else if (value >= 1)
+        {
+            value = 1;
+        }
+
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
     }
 }
