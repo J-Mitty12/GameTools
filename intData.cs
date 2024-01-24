f9f11925b91b7cb467a8b4ca279e8388eef5802e
Julian Mitton <jdmitton12@gmail.com>
2024-01-24 09:34:50 -0700

    Added Float Data Script

--- intData.cs
+++ intData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [CreateAssetMenu]
@@ -17,5 +18,21 @@ public class intData : ScriptableObject
     {
         value = num;
     }
+
+    public void ReplaceValue(int num)
+    {
+        value = num;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
+    }
+    
 
 }
