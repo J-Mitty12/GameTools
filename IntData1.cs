74c412c5ecab7aec173bf9fed0a8994f15912ce3
Julian Mitton <jdmitton12@gmail.com>
2024-01-24 10:40:04 -0700

    Created new IntData Script

---
+++ IntData1.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu]
+public class IntData1 : ScriptableObject
+{
+    public int value;
+
+    public void AddToValue(int num)
+    {
+        value += num;
+    }
+
+    public void ResetValue(int num)
+    {
+        value = num;
+    }
+
+    public void ReplaceValue(int num)
+    {
+        value = num;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
+    }
+    
+
+}
