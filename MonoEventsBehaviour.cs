4ab98cfc325b4c5a929700a06e2836452afd65f5
Julian Mitton <jdmitton12@gmail.com>
2024-01-29 08:44:14 -0700

    Added Particle System, Validate, and Collision Enter to Unity Events

--- MonoEventsBehaviour.cs
+++ MonoEventsBehaviour.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class MonoEventsBehaviour : MonoBehaviour
 {
-    public UnityEvent startEvent, awakeEvent, disableEvent;
+    public UnityEvent startEvent, awakeEvent, disableEvent, validateEvent;
+    public float holdTime;
+    private float speed = 3f;
+    
     private void Awake()
     {
         awakeEvent.Invoke();
@@ -18,5 +23,41 @@ public class MonoEventsBehaviour : MonoBehaviour
     private void OnDisable()
     {
         disableEvent.Invoke();
+    }
+
+    private void OnValidate()
+    {
+        validateEvent.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime *speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = speed * -1;
+    }
+
+    private void OnParticleTrigger()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+
+        List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+        List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(0, 255, 0, 255);
+            exit[i] = p;
+        }
+        
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
     }
 } 
